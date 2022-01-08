aa811ae68f4ae7c65a802ca71822ca5879e38119
alazar-des <alazar.des@gmail.com>
2022-01-08 18:33:33 +0300

    encrypt password

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+""" Password encription
+"""
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """ Encrypting passwords with bcrypt package
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ check if password is valid
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
