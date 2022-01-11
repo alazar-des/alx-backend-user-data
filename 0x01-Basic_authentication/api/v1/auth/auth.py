9778ea85b5e9defee5d892bc183675c1c16c0245
alazar-des <alazar.des@gmail.com>
2022-01-11 11:41:49 +0300

    allow * at the end of excluded paths

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@
 
 from flask import request
 from typing import List, TypeVar
+import re
 
 
 class Auth:
@@ -16,6 +17,11 @@ class Auth:
             return True
         if path in excluded_paths or path + "/" in excluded_paths:
             return False
+
+        for ep in excluded_paths:
+            pattern = "^" + ep
+            if re.match(pattern, path):
+                return False
         return True
 
     def authorization_header(self, request=None) -> str:
