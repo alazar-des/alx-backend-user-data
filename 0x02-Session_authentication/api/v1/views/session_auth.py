4d20a81fadd6d18e27598c415d4b77d0497b17a8
alazar-des <alazar.des@gmail.com>
2022-01-13 10:41:09 +0300

    pycodestyle

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -16,15 +16,15 @@ def login() -> str:
     """
     email = request.form.get("email")
     if email is None or email == "":
-        return jsonify({ "error": "email missing" }), 400
+        return jsonify({"error": "email missing"}), 400
     pwd = request.form.get("password")
     if pwd is None or pwd == "":
-        return jsonify({ "error": "password missing" }), 400
+        return jsonify({"error": "password missing"}), 400
     user = User.search({'email': email})
     if not user:
-        return jsonify({ "error": "no user found for this email" }), 404
+        return jsonify({"error": "no user found for this email"}), 404
     if not user[0].is_valid_password(pwd):
-        return jsonify({ "error": "wrong password" }), 401
+        return jsonify({"error": "wrong password"}), 401
     else:
         from api.v1.app import auth
         session_id = auth.create_session(user[0].id)
