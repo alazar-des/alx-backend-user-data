d990a05ac2a743af60c3af5fcd20267661f0baf8
alazar-des <alazar.des@gmail.com>
2022-01-13 10:08:53 +0300

    authentication base class

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""template for all authentication system
+"""
+from os import getenv
+from flask import request
+from typing import List, TypeVar
+import re
+
+
+class Auth:
+    """ Auth template class
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """require athentication
+        """
+        if path is None or excluded_paths is None or not excluded_paths:
+            return True
+        if path in excluded_paths or path + "/" in excluded_paths:
+            return False
+
+        for ep in excluded_paths:
+            if ep[-1] == "*":
+                pattern = "^" + ep
+                if re.match(pattern, path):
+                    return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """authorization header
+        """
+        if request is not None:
+            return request.headers.get('Authorization', None)
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """current user
+        """
+        return None
+
+    def session_cookie(self, request=None):
+        """Return cookie value from a request
+        """
+        if request is None:
+            return None
+        _my_session_id = getenv("SESSION_NAME")
+        return request.cookies.get(_my_session_id)
