c25e215d881f6df30fcef0100dc3f535b63bdb91
alazar-des <alazar.des@gmail.com>
2022-01-19 14:06:34 +0300

    flask app

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+"""flask app
+"""
+from flask import Flask, request, jsonify, abort, redirect
+from auth import Auth
+from sqlalchemy.orm.exc import NoResultFound
+
+
+AUTH = Auth()
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    """route to
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def users():
+    """register user
+    """
+    try:
+        AUTH.register_user(request.form['email'],
+                           request.form['password'])
+        return jsonify({"email": request.form['email'],
+                        "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    """login
+    """
+    if AUTH.valid_login(request.form['email'], request.form['password']):
+        session_id = AUTH.create_session(request.form['email'])
+        resp = jsonify({"email": request.form['email'],
+                        "message": "logged in"})
+        resp.set_cookie("session_id", session_id)
+        return resp
+    abort(401)
+
+
+@app.route('/sessions', methods=['DELETE'])
+def logout():
+    """logout
+    """
+    session_id = request.cookies.get('session_id')
+    try:
+        usr = AUTH.get_user_from_session_id(session_id)
+        AUTH.destroy_session(usr.id)
+        return redirect('/')
+    except NoResultFound:
+        abort(403)
+
+
+@app.route('/profile', methods=['GET'])
+def profile():
+    """profile route
+    """
+    session_id = request.cookies.get('session_id')
+    try:
+        usr = AUTH.get_user_from_session_id(session_id)
+        return jsonify({"email": usr.email}), 200
+    except NoResultFound:
+        abort(403)
+
+
+@app.route('/reset_password', methods=['POST', 'PUT'])
+def get_reset_password_token():
+    """get reset password token
+    """
+    if request.method == 'POST':
+        try:
+            reset_token = AUTH.get_reset_password_token(request.form['email'])
+            return jsonify({"email": request.form['email'],
+                            "reset_token": reset_token}), 200
+        except ValueError:
+            abort(403)
+    else:
+        try:
+            AUTH.update_password(request.form['reset_token'],
+                                 request.form['password'])
+            return jsonify({"email": request.form['email'],
+                            "message": "Password updated"}), 200
+        except ValueError:
+            abort(403)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
